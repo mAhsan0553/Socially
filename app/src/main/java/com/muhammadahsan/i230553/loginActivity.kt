60dcb53d3c56e5d64199dd8dac733bfc7a6b3c8a
Muhammad AHSAN <ahsankhetran72@gmail.com>
2025-09-09 11:40:36 +0500

    feat: Add before login screen and navigation

--- app/src/main/java/com/muhammadahsan/i230553/loginActivity.kt
+++ app/src/main/java/com/muhammadahsan/i230553/loginActivity.kt
@@ -1,31 +1,27 @@
 package com.muhammadahsan.i230553
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class beforeloginActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_beforelogin)
+        setContentView(R.layout.activity_login)
 
+        var back_arrow = findViewById<ImageView>(R.id.back_arrow)
+        back_arrow.setOnClickListener {
+            finish()
+        }
         var signup = findViewById<TextView>(R.id.sign_up)
         signup.setOnClickListener {
             var intent = Intent(this, CreateAccountActivity::class.java)
-            startActivity(intent)
-        }
-
-        var login = findViewById<TextView>(R.id.switch_account_btn)
-        login.setOnClickListener {
-            var intent = Intent(this, loginActivity::class.java)
             startActivity(intent)
         }
 
