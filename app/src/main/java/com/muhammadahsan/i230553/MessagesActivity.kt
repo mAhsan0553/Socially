7a98809f6819bbd010e83bf67d009be553583c4d
Muhammad AHSAN <ahsankhetran72@gmail.com>
2025-09-14 08:21:15 +0500

    feat: Implement chat screen UI and navigation

--- app/src/main/java/com/muhammadahsan/i230553/MessagesActivity.kt
+++ app/src/main/java/com/muhammadahsan/i230553/MessagesActivity.kt
@@ -3,6 +3,7 @@ package com.muhammadahsan.i230553
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -37,6 +38,45 @@ class MessagesActivity : AppCompatActivity() {
             var intent = android.content.Intent("android.intent.action.PICK")
             intent.type = "image/*"
             startActivity(intent)
+        }
+
+        var chat1 = findViewById<RelativeLayout>(R.id.chat1)
+        chat1.setOnClickListener {
+            var intent = android.content.Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+
+        var chat2 = findViewById<RelativeLayout>(R.id.chat2)
+        chat2.setOnClickListener {
+            var intent = android.content.Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+
+        var chat3 = findViewById<RelativeLayout>(R.id.chat3)
+        chat3.setOnClickListener {
+            var intent = android.content.Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+
+        }
+        var chat4 = findViewById<RelativeLayout>(R.id.chat4)
+        chat4.setOnClickListener {
+            var intent = android.content.Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        var chat5 = findViewById<RelativeLayout>(R.id.chat5)
+        chat5.setOnClickListener {
+            var intent = android.content.Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        var chat6 = findViewById<RelativeLayout>(R.id.chat6)
+        chat6.setOnClickListener {
+            var intent = android.content.Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+
         }
 
     }
